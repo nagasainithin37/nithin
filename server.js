78e4a7ba61916ba0df35d3bc9d489e1b7105667e
nagasainithin37 <nagasainithin37@gmail.com>
2023-07-13 15:09:59 +0530

    gref

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const exp=require('express')
 const app=exp()
 var cors = require('cors')
 // var app = express()
- 
+ app.use(exp.json())
 app.use(cors())
 app.get('/get',(req,res)=>{
     res.send({message:'hello'})
